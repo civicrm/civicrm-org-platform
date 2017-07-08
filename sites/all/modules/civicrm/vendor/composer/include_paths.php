22867fd871ba46d4b861a5d7359308087adff44d
Mathieu Lutfy <mathieu@symbiotic.coop>
2017-07-08 04:25:01 +0000

    CiviCRM 4.7.21.

--- sites/all/modules/civicrm/vendor/composer/include_paths.php
+++ sites/all/modules/civicrm/vendor/composer/include_paths.php
@@ -8,6 +8,9 @@ $baseDir = dirname($vendorDir);
 return array(
     $vendorDir . '/tecnickcom',
     $vendorDir . '/pear/pear_exception',
-    $vendorDir . '/pear/validate_finance_creditcard',
+    $vendorDir . '/pear/net_socket',
+    $vendorDir . '/pear/net_smtp',
+    $vendorDir . '/pear/auth_sasl',
     $vendorDir . '/phpseclib/phpseclib/phpseclib',
+    $vendorDir . '/pear/validate_finance_creditcard',
 );
