b8e531c0e92bf29322e631cc7836ff387a8c7ec9
Mathieu Lutfy <mathieu@bidon.ca>
2017-09-21 01:13:39 +0000

    Upgrade CiviCRM 4.7.24.

--- sites/all/modules/civicrm/vendor/composer/autoload_static.php
+++ sites/all/modules/civicrm/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit15273363a8ff4b798eb304902c5f2b23
+class ComposerStaticInit98885188e2a7f5104d0634cf8bd60247
 {
     public static $files = array (
         'decc78cc4436b1292c6c0d151b19445c' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
@@ -22,6 +22,10 @@ class ComposerStaticInit15273363a8ff4b798eb304902c5f2b23
         array (
             'PhpOffice\\PhpWord\\' => 18,
             'PhpOffice\\Common\\' => 17,
+        ),
+        'F' => 
+        array (
+            'FontLib\\' => 8,
         ),
         'D' => 
         array (
@@ -53,6 +57,10 @@ class ComposerStaticInit15273363a8ff4b798eb304902c5f2b23
         'PhpOffice\\Common\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpoffice/common/src/Common',
+        ),
+        'FontLib\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phenx/php-font-lib/src/FontLib',
         ),
         'Dompdf\\' => 
         array (
@@ -106,6 +114,10 @@ class ComposerStaticInit15273363a8ff4b798eb304902c5f2b23
             array (
                 0 => __DIR__ . '/..' . '/phenx/php-svg-lib/src',
             ),
+            'Sabberworm\\CSS' => 
+            array (
+                0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/lib',
+            ),
         ),
         'P' => 
         array (
@@ -126,9 +138,9 @@ class ComposerStaticInit15273363a8ff4b798eb304902c5f2b23
         array (
             'Net' => 
             array (
-                0 => __DIR__ . '/..' . '/pear/net_socket',
-                1 => __DIR__ . '/..' . '/pear/net_smtp',
-                2 => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib',
+                0 => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib',
+                1 => __DIR__ . '/..' . '/pear/net_socket',
+                2 => __DIR__ . '/..' . '/pear/net_smtp',
             ),
         ),
         'M' => 
@@ -140,10 +152,6 @@ class ComposerStaticInit15273363a8ff4b798eb304902c5f2b23
         ),
         'F' => 
         array (
-            'FontLib\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/phenx/php-font-lib/src',
-            ),
             'File' => 
             array (
                 0 => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib',
@@ -323,10 +331,10 @@ class ComposerStaticInit15273363a8ff4b798eb304902c5f2b23
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit15273363a8ff4b798eb304902c5f2b23::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit15273363a8ff4b798eb304902c5f2b23::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit15273363a8ff4b798eb304902c5f2b23::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit15273363a8ff4b798eb304902c5f2b23::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit98885188e2a7f5104d0634cf8bd60247::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit98885188e2a7f5104d0634cf8bd60247::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit98885188e2a7f5104d0634cf8bd60247::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit98885188e2a7f5104d0634cf8bd60247::$classMap;
 
         }, null, ClassLoader::class);
     }
