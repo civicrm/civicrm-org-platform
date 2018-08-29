8e38edee515d75689cd85741aaab8e5af2073651
Mathieu Lutfy <mathieu@civicrm.org>
2018-08-29 15:12:36 +0000

    Upgrade to CiviCRM 5.5.beta1 (for newer iATS and self-service,)

--- sites/all/modules/civicrm/vendor/autoload.php
+++ sites/all/modules/civicrm/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitbbfe77676f41b60311acdd87c7a0fed6::getLoader();
+return ComposerAutoloaderInite1db189a4a0f6b046c28cdf98084f22e::getLoader();
