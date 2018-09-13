9e9280bf6bce89a8b90ae19ee7499774ad19dfe3
Mathieu Lutfy <mathieu@civicrm.org>
2018-09-13 18:55:03 +0000

    Upgrade to CiviCRM 5.5.1

--- sites/all/modules/civicrm/civicrm-version.php
+++ sites/all/modules/civicrm/civicrm-version.php
@@ -1,7 +1,7 @@
 <?php
 /** @deprecated */
 function civicrmVersion( ) {
-  return array( 'version'  => '5.5.beta1',
+  return array( 'version'  => '5.5.1',
                 'cms'      => 'Drupal',
                 'revision' => '' );
 }
--- sites/all/modules/civicrm/vendor/autoload.php
+++ sites/all/modules/civicrm/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite1db189a4a0f6b046c28cdf98084f22e::getLoader();
+return ComposerAutoloaderInit61d8e138f140ca7ed358873ad7e10d15::getLoader();
